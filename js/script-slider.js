335cf9cd9050d9cf3555ac8d60302d7e9c7110bb
nvostokov <103598346+nvostokov@users.noreply.github.com>
2023-01-13 10:57:20 +0300

    Add files via upload

---
+++ js/script-slider.js
@@ -0,0 +1,55 @@
+new Swiper('.image-slider', {
+    /* стрелки */
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    /* Навигация */
+    pagination: {
+        el: '.swiper-pagination',
+        /* Буллеты */
+        clickable: true,
+        /* Динамические буллеты */
+        dynamicBullets: true,
+        // Кастомные буллеты
+        /* renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>'}; */
+        },
+/*     slidesPerView: 3, */
+    loop: true,
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+    speed: 3000,
+
+    breakpoints: {// настройки для разных разрешений
+        600: {
+            slidesPerView: 1,
+            /* spaceBetween: 20, */
+        },
+        991: {
+            slidesPerView: 2,
+            /* spaceBetween: 20, */
+        },
+        1200: {
+            slidesPerView: 3,
+            /* spaceBetween: 20, */
+        }
+    }
+        /* 3d effects */
+    /* effect: 'cube', */
+/*     cubeEffect: {
+        slideShadows: true,
+        shadow: true,
+        shadowOffset: 20,
+        shadowScale: 0.94
+    } */
+    /* Эффект потока */
+    /* effects: 'coverflow',
+    coverflowEffect: {
+        rotate: 20,
+        stretch: 50,
+        slideShadows: true,
+    }, */
+    });
